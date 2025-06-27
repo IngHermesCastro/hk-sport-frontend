9b8972037382c32210e939e12f3c65e23882fa68
Hermes Castro <1hermescastro@gmail.com>
2025-06-27 09:49:24 -0500

    rutas
    update rutas

---
+++ src/environments/dist/environment.development.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+exports.environment = void 0;
+exports.environment = {
+    baseUrl: 'https://hk-sport-shop.onrender.com/api'
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  baseUrl: 'http://localhost:3000/api',
+  // baseUrl: 'http://localhost:3000/api',
+  baseUrl: 'https://hk-sport-shop.onrender.com/api',
 
 };
